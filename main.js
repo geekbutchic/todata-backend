9d3d8dbd4f3d1a31b7b92912baa1e18df0d3b09a
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-10-27 22:19:16 -0400

    finish all solutions

--- main.js
+++ main.js
@@ -2,31 +2,107 @@
  * HELPER FUNCTIONS *
  ********************/
 
+const getTodoName = function (element) {
+  return element.text 
+}
 
+const getPriority = function (element) {
+  return element.priority
+}
 
+const isComplete = function (element) {
+  return element.complete === true
+}
 
+const isHighPriority = function (element) {
+  return element.priority > 1
+}
 
+const namePlusPriority = function (element) {
+  let newPriority = '';
 
+  element.priority === 2 ? newPriority = 'High' : newPriority = 'Low'
 
-
-
+  return element.text + ' - ' + newPriority
+}
 
 /***********************
  * ITERATION FUNCTIONS *
  ***********************/
 
+const names = function (arr) {
+  const newNames = [];
 
+  for (const element of arr) {
+    newNames.push(getTodoName(element));
+  }
+  return newNames;
+}
 
+const priorities = function (arr) {
+  const newPriorities = [];
 
+  for (const element of arr) {
+    newPriorities.push(getPriority(element));
+  }
 
+  return newPriorities;
+}
 
+const namesAndPriorities = function (arr) {
+  const newList = [];    
 
+  for (const element of arr) {
+    newList.push(namePlusPriority(element));
+  }
 
+  return newList;
+}
 
+const justComplete = function (arr) {
+  const notComplete = [];
 
+  for (const element of arr) {
+    if (isComplete(element)) {
+      notComplete.push(element)
+    }
+  }
+  return notComplete;
+}
 
+const priority2Only = function (arr) {
+  const highPriority = [];
 
+  for (const element of arr) {
+    if (isHighPriority(element)) {
+      highPriority.push(element)
+    }
+  }
+  return highPriority;
+}
 
+const priority1Only = function (arr) {
+  const lowPriority = [];
+
+  for (const element of arr) {
+    if (!isHighPriority(element)) {
+      lowPriority.push(element)
+    }
+  }
+  return lowPriority;
+}
+
+const justNotComplete = function (arr) {
+  const notCompleted = [];
+
+  for (const element of arr) {
+    if (!isComplete(element)) {
+      notCompleted.push(element)
+    }
+  }
+
+    return notCompleted;
+}
 
 // Our code below
 
